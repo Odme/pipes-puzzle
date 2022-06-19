41343007ca4775a8e1f103a1619ffe6cf60966ba
Omar Mejía <odme1994@gmail.com>
2022-06-19 00:07:18 -0500

    created initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,31 +5,37 @@ module.exports = {
     node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     'react',
+    '@typescript-eslint',
   ],
   rules: {
     'no-unused-vars': 'warn',
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies:
-        [
-          '**/*.spec.js',
-          '**/*.test.js',
-          'vite.config.js',
-        ],
-    }],
     'linebreak-style': [0, 'error', 'windows'],
     'react/react-in-jsx-scope': 'off',
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
---
+++ src/api/socket.ts
@@ -0,0 +1,32 @@
+export const connectSocket = (): void => {
+  const socket = new WebSocket('wss://hometask.eg1236.com/game-pipes/');
+  socket.onopen = function (_e) {
+    alert('[open] Connection established');
+    alert('Sending to server');
+    socket.send('new 1');
+    socket.send('map');
+  };
+
+  socket.onmessage = function (event) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    alert(`[message] Data received from server: ${event.data}`);
+    console.log(event.data);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      alert(
+        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`,
+      );
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      alert('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error: any) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions
+    alert(`[error] ${error.message || ''}`);
+  };
+};
